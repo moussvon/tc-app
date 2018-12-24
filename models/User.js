faa7d6922d78558ddd2f47db19699ab56ff84056
Mehdi Mitiche <gm_mitiche@esi.dz>
2018-12-24 05:30:28 +0100

    hashing a password before saving Added

--- models/User.js
+++ models/User.js
@@ -3,10 +3,16 @@ import TodoSchema from './Todo'
 import DefiSchema from './Todo'
 import ArticleSchema from './Article'
 
+const bcrypt = require('bcryptjs')
+
 
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: 'User name is required'
+    },
     nom: {
         type: String,
         required: 'title is required'
@@ -15,14 +21,14 @@ const UserSchema = new Schema({
         type: String,
         required: 'author is required'
     },
-    email:{
-    	type : Date, 
-    	default : Date.now
+    email: {
+        type: String,
+        required: 'author is required'
     },
     birthdate: {
         type: Date
     },
-    todo:{
+    todo: {
         type: [TodoSchema]
     },
     defi: {
@@ -50,5 +56,17 @@ const UserSchema = new Schema({
         type: [UserSchema]
     }
 })
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function(next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
 
 module.exports = UserSchema
